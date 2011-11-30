427df061a103eecd3b7d8418977fa60bbf323069
Val L33 <val@neekware.com>
2011-11-30 10:04:28 -0500

    move setting files

--- emailmgr/__init__.py
+++ emailmgr/__init__.py
@@ -3,5 +3,5 @@ try:
     _s = os.environ['DJANGO_SETTINGS_MODULE']
 except KeyError:
     # DJANGO_SETTINGS_MODULE should have been set by now, if not, we must be in test mode
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'emailmgr.testsettings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'emailmgr.tests.testsettings'
 
